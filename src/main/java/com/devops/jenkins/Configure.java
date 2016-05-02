e71dcc94c7f9cff75b0e5a691a1ee2c37931aad8
bharani chennu <bharanic404@gmail.com>
2016-05-02 16:35:21 +0530

    file checkin for jenkins test

---
+++ src/main/java/com/devops/jenkins/Configure.java
@@ -0,0 +1,12 @@
+package com.devops.jenkins;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Configure{
+
+
+public void testCollection(){
+List<String> list=new ArrayList<String>();
+}
+}
